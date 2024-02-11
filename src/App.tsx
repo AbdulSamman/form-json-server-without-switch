50299c92db633b29e73fe95ec186c12d715caa19
AbdulSamman <sammanab@outlook.de>
2024-02-11 19:53:08 +0100

    form styled

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,24 @@ function App() {
   return (
     <div>
       <h1>To Do App</h1>
-      <p>WElcome to this site.</p>
+      <p>Welcome to this site.</p>
+      <form>
+        <fieldset>
+          <legend>New Subject</legend>
+          <div className="row">
+            <label>Title</label>
+            <div>
+              <input type="text" />
+            </div>
+          </div>
+          <div className="row">
+            <label>Description</label>
+            <div>
+              <textarea />
+            </div>
+          </div>
+        </fieldset>
+      </form>
     </div>
   );
 }

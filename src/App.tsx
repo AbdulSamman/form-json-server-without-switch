84045bb8c2e9986449c1a821748f9ad78aa8094f
AbdulSamman <sammanab@outlook.de>
2024-02-11 22:44:18 +0100

    created json-server

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,69 @@
 import "./App.scss";
+import { useState } from "react";
+
+const _formData = {
+  jobTitle: "",
+  description: "",
+};
 
 function App() {
+  const [formData, setFormData] = useState(_formData);
+
+  const handleFieldChange = (e: any, fieldName: string) => {
+    const value = e.target.value;
+    //statevariable value ändern
+    // einzel
+    //formData.jobTitle = value;
+    //setFormData({ ...formData });
+    //mehrere
+    switch (fieldName) {
+      case "jobTitle":
+        formData.jobTitle = value;
+        break;
+      case "description":
+        formData.description = value;
+        break;
+    }
+    setFormData({ ...formData });
+  };
+
   return (
-    <div>
+    <div className="App">
       <h1>To Do App</h1>
-      <p>Welcome to this site.</p>
-      <form>
-        <fieldset>
-          <legend>New Subject</legend>
-          <div className="row">
-            <label>Title</label>
-            <div>
-              <input type="text" />
+
+      <section>
+        <form>
+          <fieldset>
+            <legend>New Subject</legend>
+            <div className="row">
+              <label>Title</label>
+              <div>
+                <input
+                  type="text"
+                  value={formData.jobTitle}
+                  onChange={(e) => handleFieldChange(e, "jobTitle")}
+                />
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <label>Description</label>
-            <div>
-              <textarea />
+            <div className="row">
+              <label>Description</label>
+              <div>
+                <textarea
+                  spellCheck="false"
+                  value={formData.description}
+                  onChange={(e) => handleFieldChange(e, "description")}
+                />
+              </div>
             </div>
-          </div>
-        </fieldset>
-      </form>
+            <div className="buttonArea">
+              <button>Save</button>
+            </div>
+          </fieldset>
+        </form>
+        <div className="debuggingArea">
+          <pre className="showData">{JSON.stringify(formData, null, 2)}</pre>
+        </div>
+      </section>
     </div>
   );
 }
